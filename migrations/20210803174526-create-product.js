6998631d14a42e18a63a50c4a6782ab5767fd2c2
notblessy <66545996+notblessy@users.noreply.github.com>
2021-08-07 01:12:28 +0700

    Change product id (#6)

--- migrations/20210803174526-create-product.js
+++ migrations/20210803174526-create-product.js
@@ -4,9 +4,9 @@ module.exports = {
     await queryInterface.createTable('products', {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
+        unique: true,
       },
       sku: {
         type: Sequelize.STRING,
